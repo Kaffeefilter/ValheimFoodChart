314ce934994a7434574510ed1509ec51fd95e796
Kaffeefilter <thoutrou@gmail.com>
2021-11-17 01:25:05 +0100

    sliders buttons and healing cb now working

--- valheimfood.py
+++ valheimfood.py
@@ -9,44 +9,42 @@ import matplotlib
 matplotlib.use('TkAgg')
 
 
-def sortFoodKey(food, healthweight = 5, staminaweight = 5, tickweight = 1):
+def sortFoodKey(food, healthweight = 100, staminaweight = 100, tickweight = 1):
     #baseline wolf jerky: hp = 33, stamina = 33, hpptick = 3 => hp == stamina == hpptick * 11
-    health_weighted = (food[0]['health']+ food[1]['health'] + food[2]['health']) * healthweight
-    stamina_weighted = (food[0]['stamina']+ food[1]['stamina'] + food[2]['stamina']) * staminaweight 
-    hpptick_weighted = (food[0]['hp/tick']+ food[1]['hp/tick'] + food[2]['hp/tick']) * 11 * tickweight
+    health_weighted = (food[0]['health'] + food[1]['health'] + food[2]['health']) * healthweight
+    stamina_weighted = (food[0]['stamina'] + food[1]['stamina'] + food[2]['stamina']) * staminaweight 
+    hpptick_weighted = (food[0]['hp/tick'] + food[1]['hp/tick'] + food[2]['hp/tick']) * 11 * tickweight
     return health_weighted + stamina_weighted + hpptick_weighted
     
 
-def generateGraph(foods, n = 5, healthweight = 1, staminaweight = 1, tickweight = 1):
+def generateGraph(foods, n = 5, healthweight = 100, staminaweight = 100, tickweight = 1):
 
     combinedFood = []
-
     for foodcomb in combinations(foods, 3):
         combinedFood.append([foodcomb[0], foodcomb[1], foodcomb[2]])
 
     combinedFood.sort(key=lambda x: sortFoodKey(x, healthweight, staminaweight, tickweight), reverse=True)
     #print(tabulate(combinedFood[:10]))
 
-    hpBars1 = [combination[0]['health'] for combination in combinedFood][:5]
-    hpBars2 = [combination[1]['health'] for combination in combinedFood][:5]
-    hpBars3 = [combination[2]['health'] for combination in combinedFood][:5]
+    hpBars1 = [combination[0]['health'] for combination in combinedFood][:n]
+    hpBars2 = [combination[1]['health'] for combination in combinedFood][:n]
+    hpBars3 = [combination[2]['health'] for combination in combinedFood][:n]
     # Heights of bars1 + bars2
     hpBars = np.add(hpBars1, hpBars2).tolist()
 
-    staBars1 = [combination[0]['stamina'] for combination in combinedFood][:5]
-    staBars2 = [combination[1]['stamina'] for combination in combinedFood][:5]
-    staBars3 = [combination[2]['stamina'] for combination in combinedFood][:5]
+    staBars1 = [combination[0]['stamina'] for combination in combinedFood][:n]
+    staBars2 = [combination[1]['stamina'] for combination in combinedFood][:n]
+    staBars3 = [combination[2]['stamina'] for combination in combinedFood][:n]
     staBars = np.add(staBars1, staBars2).tolist()
 
-    tickBars1 = [combination[0]['hp/tick'] for combination in combinedFood][:5]
-    tickBars2 = [combination[1]['hp/tick'] for combination in combinedFood][:5]
-    tickBars3 = [combination[2]['hp/tick'] for combination in combinedFood][:5]
+    tickBars1 = [combination[0]['hp/tick'] for combination in combinedFood][:n]
+    tickBars2 = [combination[1]['hp/tick'] for combination in combinedFood][:n]
+    tickBars3 = [combination[2]['hp/tick'] for combination in combinedFood][:n]
     tickBars = np.add(tickBars1, tickBars2).tolist()
 
-    names = []
-    for combination in combinedFood[:5]:
-        names.append(f"{combination[0]['name']} + {combination[1]['name']} + {combination[2]['name']}")
-
+    """ names = []
+    for combination in combinedFood[:n]:
+        names.append(f"{combination[0]['name']} + {combination[1]['name']} + {combination[2]['name']}") """
     numbers = list(range(1, n+1))
 
     barWidth = 0.25
@@ -67,9 +65,9 @@ def generateGraph(foods, n = 5, healthweight = 1, staminaweight = 1, tickweight 
     plt.bar(r3, tickBars2, bottom=tickBars1, color='#557f2d', width=barWidth, edgecolor='white')
     plt.bar(r3, tickBars3, bottom=tickBars, color='#2d7f5e', width=barWidth, edgecolor='white')
 
-    plt.xticks(r2, names, rotation='vertical')
+    plt.xticks(r2, numbers)
 
-    return plt.gcf()
+    return (plt.gcf(), combinedFood[:n])
 
 
 def drawFigure(canvas, figure):
@@ -78,19 +76,42 @@ def drawFigure(canvas, figure):
     figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
     return figure_canvas_agg
 
-def createCell(key, i):
-    #return sg.Text(text=f"{i if key == 'NUM' else ''}", key=f"_TABLE_{key}_{i}_")
-    return sg.Text(text=f"{i if key == 'NUM' else f'_TABLE_{key}_{i}_'}", key=f"_TABLE_{key}_{i}_")
 
-def updateGraph(foods, n = 5, healthweight = 100, staminaweight = 100, healingweight = 1):
-    pass
+def createCell(key, i):
+    return sg.Text(text=f"{i if key == 'NUM' else ''}", key=f"_TABLE_{key}_{i}_")
+    #return sg.Text(text=f"{i if key == 'NUM' else f'_TABLE_{key}_{i}_'}", key=f"_TABLE_{key}_{i}_")
+
+
+def updateGraph(window, foods, n = 5, healthweight = 100, staminaweight = 100, healingweight = 1):
+    graph, combinedFood = generateGraph(foods, n, healthweight, staminaweight, healingweight)
+
+    for i in range(1, 21):
+        for key in ('NUM', 'NAME', 'HEALTH', 'STAMINA', 'HEALING'):
+            window[f'_TABLE_{key}_{i}_'].update("")
+            if(i <= n):
+                match key:
+                    case 'NUM':
+                        window[f'_TABLE_{key}_{i}_'].update(i)
+                    case 'NAME':
+                        window[f'_TABLE_{key}_{i}_'].update(f"{combinedFood[i-1][0]['name']} + {combinedFood[i-1][1]['name']} + {combinedFood[i-1][2]['name']}")
+                    case 'HEALTH':
+                        window[f'_TABLE_{key}_{i}_'].update(combinedFood[i-1][0]['health'] + combinedFood[i-1][1]['health'] + combinedFood[i-1][2]['health'])
+                    case 'STAMINA':
+                        window[f'_TABLE_{key}_{i}_'].update(combinedFood[i-1][0]['stamina'] + combinedFood[i-1][1]['stamina'] + combinedFood[i-1][2]['stamina'])
+                    case 'HEALING':
+                        window[f'_TABLE_{key}_{i}_'].update(combinedFood[i-1][0]['hp/tick'] + combinedFood[i-1][1]['hp/tick'] + combinedFood[i-1][2]['hp/tick'])
+    return graph
+
+def delete_figure_agg(figure_agg):
+    figure_agg.get_tk_widget().forget()
+    plt.close('all')
 
 
 def main():
     with open("food.json") as f:
         foods = json.load(f)
     
-    graph = generateGraph(foods)
+    #graph = generateGraph(foods)
 
     table = [ [sg.Text(text="#"), sg.Text(text="Name"), sg.Text(text="HP"), sg.Text(text="Stamina"), sg.Text(text="Healing")] ]
     #table += [ [sg.Text(text=f"{i}", key=f"_TABLE_NUM_{i}_"), sg.Text(key=f"_TABLE_NAME_{i}_"), sg.Text(key=f"_TABLE_HEALTH_{i}_"), sg.Text(key=f"_TABLE_STAMINA_{i}_"), sg.Text(key=f"_TABLE_HEALING_{i}_")] for i in range(1, 21)]
@@ -112,7 +133,7 @@ def main():
 
     slider_preference = sg.Slider(key="_SLIDER_PREFERENCE_", range=(0, 180), default_value=90, orientation='horizontal', disable_number_display=True, enable_events=True)
     cb_healing = sg.Checkbox(key='_CBHEALING_', text="Healing", enable_events=True)
-    slider_elements = sg.Slider(key="_SLIDER_ELEMENTS_", range=(1,20), default_value=5, orientation='horizontal', disable_number_display=True, enable_events=True)
+    slider_elements = sg.Slider(key="_SLIDER_ELEMENTS_", range=(1, 20), default_value=5, orientation='horizontal', disable_number_display=True, enable_events=True)
 
     layout_preference = [
         [text_slider_left, text_slider_middle, text_slider_right],
@@ -129,17 +150,19 @@ def main():
             [canvas],
             [cb_meadows, cb_blackforest, cb_swamp, cb_mountain, cb_plains, cb_ocean],
             [sg.Column(layout_preference), sg.Column(layout_element_numbers)],
-            [sg.Button('Ok'), sg.Button('Reset')]
+            [sg.Button('Update'), sg.Button('Reset')]
         ]), sg.Column(table, vertical_alignment='top')]
     ]
 
     window = sg.Window("Vallheim Foods", layout, finalize=True)
 
-    drawFigure(window['_CANVAS_'].TKCanvas, graph)
+    graph = updateGraph(window, foods)
+    fig_canvas_agg = drawFigure(window['_CANVAS_'].TKCanvas, graph)
 
     healthweight = 100
     staminaweight = 100
     healingweight = 1
+    numbers = 5
 
     running = True
     while running:
@@ -150,8 +173,10 @@ def main():
             case sg.WIN_CLOSED:
                 running = False
                 #break      #would work but coud be mistaken for switch case break
-            case "Ok":
-                print(values)
+            case "Update":
+                graph = updateGraph(window, foods, int(numbers), healthweight, staminaweight, healingweight)
+                delete_figure_agg(fig_canvas_agg)
+                fig_canvas_agg = drawFigure(window['_CANVAS_'].TKCanvas, graph)
             case "Reset":
                 window['_TEXT_SLIDER_'].update("1.00:1.00")
                 window['_SLIDER_PREFERENCE_'].update(90)
@@ -167,6 +192,7 @@ def main():
                 healthweight = 100
                 staminaweight = 100
                 healingweight = 1
+                numbers = 5
             case "_SLIDER_PREFERENCE_":
                 slider_value = values['_SLIDER_PREFERENCE_'] + 10
                 healthweight = 1 if slider_value > 100 else slider_value / 100
@@ -174,6 +200,7 @@ def main():
                 window['_TEXT_SLIDER_'].update(f"{healthweight:.2f}:{staminaweight:.2f}")
             case "_SLIDER_ELEMENTS_":
                 window['_TEXT_NUMBER_'].update(f"Anzahl {values['_SLIDER_ELEMENTS_']:n}")
+                numbers = values['_SLIDER_ELEMENTS_']
             case "_CBHEALING_":
                 healingweight = 50 if values['_CBHEALING_'] else 1
 
